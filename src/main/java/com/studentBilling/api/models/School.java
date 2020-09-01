f53362b2131fa6eedf6bf7f11a814c86414625b5
AndreaNim <andriperera.98@gmail.com>
2020-09-01 17:17:40 +0530

    create school model

--- src/main/java/com/studentBilling/api/models/School.java
+++ src/main/java/com/studentBilling/api/models/School.java
@@ -1,4 +1,47 @@
 package com.studentBilling.api.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class School {
+    private int schoolId;
+    private String schoolName;
+    private String schoolType;
+
+    public School(int schoolId, String schoolName, String schoolType) {
+        this.schoolId = schoolId;
+        this.schoolName = schoolName;
+        this.schoolType = schoolType;
+    }
+
+    public School() {
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(int schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getSchoolType() {
+        return schoolType;
+    }
+
+    public void setSchoolType(String schoolType) {
+        this.schoolType = schoolType;
+    }
 }
