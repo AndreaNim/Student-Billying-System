879002b2019ff3b5c3d9ead3cf6ed760a7b07f38
AndreaNim <andriperera.98@gmail.com>
2020-09-01 17:55:13 +0530

    add tuition plan service methods

--- src/main/java/com/studentBilling/api/services/TuitionPlanService.java
+++ src/main/java/com/studentBilling/api/services/TuitionPlanService.java
@@ -1,15 +1,35 @@
 package com.studentBilling.api.services;
 
+import com.studentBilling.api.models.School;
+import com.studentBilling.api.models.Student;
+import com.studentBilling.api.models.TuitionPlan;
 import com.studentBilling.api.repositories.StudentRepository;
 import com.studentBilling.api.repositories.TuitionPlanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
 public class TuitionPlanService {
     @Autowired
     TuitionPlanRepository tuitionPlanRepository;
+
+    public List<TuitionPlan> listAllPlans() {
+        return tuitionPlanRepository.findAll();
+    }
+
+    public void addTuitionPlan(TuitionPlan tuitionPlan) {
+        tuitionPlanRepository.save(tuitionPlan);
+    }
+
+    public TuitionPlan getTuitionPlan(Integer id) {
+        return tuitionPlanRepository.findById(id).get();
+    }
+
+    public void deleteTuitionPlan(Integer id) {
+        tuitionPlanRepository.deleteById(id);
+    }
 }
