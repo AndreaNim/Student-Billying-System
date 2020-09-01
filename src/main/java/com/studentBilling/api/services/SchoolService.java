9e35f3f234b76580cc7f863ff8ba7037c16c9f03
AndreaNim <andriperera.98@gmail.com>
2020-09-01 17:52:14 +0530

    add school service methods

--- src/main/java/com/studentBilling/api/services/SchoolService.java
+++ src/main/java/com/studentBilling/api/services/SchoolService.java
@@ -1,16 +1,36 @@
 package com.studentBilling.api.services;
 
+import com.studentBilling.api.models.Payment;
+import com.studentBilling.api.models.School;
 import com.studentBilling.api.repositories.SchoolRepository;
 import com.studentBilling.api.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
 public class SchoolService {
     @Autowired
     SchoolRepository schoolRepository;
+
+    public List<School> listAllSchools() {
+        return schoolRepository.findAll();
+    }
+
+    public void addSchool(School school) {
+        schoolRepository.save(school);
+    }
+
+    public School getSchool(Integer id) {
+        return schoolRepository.findById(id).get();
+    }
+
+    public void deleteSchool(Integer id) {
+        schoolRepository.deleteById(id);
+    }
+
 
 }
