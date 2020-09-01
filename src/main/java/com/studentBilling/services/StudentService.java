1bdc3f2ac82ae825fcfc3dbd15b0c241de62d3ff
AndreaNim <andriperera.98@gmail.com>
2020-09-01 09:57:39 +0530

    Create student service

--- src/main/java/com/studentBilling/services/StudentService.java
+++ src/main/java/com/studentBilling/services/StudentService.java
@@ -1,5 +1,32 @@
 package com.studentBilling.services;
 
+import com.studentBilling.models.Student;
+import com.studentBilling.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
 public class StudentService {
-    
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> listAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void registerStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    public Student getStudent(Integer id) {
+        return studentRepository.findById(id).get();
+    }
+
+    public void deleteStudent(Integer id) {
+        studentRepository.deleteById(id);
+    }
 }
